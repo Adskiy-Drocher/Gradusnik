ac242f5d33b516704fcc72ba3ab762d238fb1d78
Adskiy-Drocher <95124021+Adskiy-Drocher@users.noreply.github.com>
2022-02-12 18:41:27 +0400

    Add files via upload

--- GUI database.py
+++ GUI database.py
@@ -6,6 +6,7 @@ from Trainner_script import Trainer
 from Raspoznal import Recognizer
 import os
 import json
+import shutil
 
 #Создания ока приложения Tkinter
 window = Tk()
@@ -17,7 +18,15 @@ window.geometry('855x550')
 
 
 #Здесь функции по созданию базы и архива, и перехода по страницам
-
+def delete():
+    select = base.curselection()
+    select_text = base.get(first=select)
+    pupil = session.query(User, User.image_path).filter_by(id=select_text[0:3]).first()
+    print(f'Ученик ------{pupil} {pupil[0]}')
+    shutil.rmtree(pupil[1])
+    session.delete(pupil[0])
+    session.commit()
+    base.delete(select)
 
 def recognize():
     with open('data') as data_file:
@@ -44,24 +53,24 @@ def choose_dir():
         json.dump(data, file, indent=4, ensure_ascii=False)
     btn5.config(text='Изменить папку архива')
 
-def make_image():
-    global val_direct
-    y = session.query(User.id)[-1]
-    maker = Image_maker()
-    maker.take_image(user_id=y[0], path=direct.get())
-    user_f_o.set('Пользователь не добавлен')
-    name.set('')
-    surname.set('')
-    trainer = Trainer()
-    path = val_direct + '/' + str(y[0])
-    print(f'Путь {path}')
-    print(f'Файлы папки юзера{os.listdir(path)}')
-    trainer.getImagesAndLabels(path)
-
 
 def create_user_make_im():
+    if len(name.get()) == 0 or len(surname.get()) == 0:
+        print(len(name.get()))
+        print(len(surname.get()))
+        user_f_o.set('Одно из полей/оба пусты!')
+        print("""
+        ПИЗДЕЦ
+        ПИЗДЕЦ
+        ПИЗДЕЦ
+        ПИЗДЕЦ
+        ПИЗДЕЦ
+        """)
+        return
     user_name = name.get()
+    print('Хуйняяя' ,user_name)
     user_surname = surname.get()
+    print('Хуйняяя 2', user_surname)
     global val_direct
     maker = Image_maker()
     maker.take_image(len(session.query(User).all())+1, path=direct.get())
@@ -88,23 +97,32 @@ def page3():
         el.pack_forget()
     btn_back.pack()
     for instance in session.query(User).all():
-        base.insert(END, f'Ф.И: {instance.surname} {instance.name}; сост. здоровья: {instance.health}; Фото: {instance.image_path}')
-    base.pack(side=LEFT, fill=Y)
-    sb.pack(side=RIGHT, fill=Y)
+        base.insert(END, f'{instance.id}  Ф.И: {instance.surname} {instance.name}; сост. здоровья: {instance.health}; Фото: {instance.image_path}')
+    for el in page3:
+        if str(el) == '.!listbox':
+            el.pack(side=LEFT, fill=Y)
+        elif el == '.!scrollbar':
+            el.pack(side=RIGHT, fill=Y)
+        else:
+            el.pack()
+    #del_lab.pack()
+    #del_btn.pack()
+    #base.pack(side=LEFT, fill=Y)
+    #sb.pack(side=RIGHT, fill=Y)
     base.config(yscrollcommand=sb.set)
     sb.config(command=base.yview)
     active_page = page3
 
 def page2():
-    try:
-        with open('data') as data_file:
-            data = json.load(data_file)
-    except:
-        data = {
-            'dir_path': 'папка не выбранна'
-        }
+    with open('data') as data_file:
+        data = json.load(data_file)
     global page1, page2, active_page
-    direct.set(data["dir_path"])
+    if len(data["dir_path"]) > 0:
+        direct.set(data["dir_path"])
+        btn5.config(text='Изменить папку')
+    elif len(data["dir_path"]) == 0:
+        direct.set('Папка не выбрана')
+        btn5.config(text='Выбрать окончательную папку')
     for el in page1:
         el.pack_forget()
     for el in page2:
@@ -112,8 +130,16 @@ def page2():
     btn_back.pack()
     active_page = page2
 
-
-
+def pack_page1():
+    with open('data') as data_file:
+        data = json.load(data_file)
+    for el in page1:
+        print(el)
+        el.pack()
+    if len(data["dir_path"]) > 0:
+        ras_str.set(f'Папка распознования {data["dir_path"]}')
+    elif len(data["dir_path"]) == 0:
+        ras_str.set('Вы еще ничего не добавляли в базу данных')
 
 def page1_back():
     global active_page, page1
@@ -143,14 +169,14 @@ col2 = '#555'
 btn_back = Button(text='На главную', command=page1_back, font=font, fg=col1, bg=col2)
 
 #Страница №1
+ras_str = StringVar()
+ras_lab = Label(textvariable=ras_str, font=font)
 btn1 = Button(text='Открыть базу данных', command=page3, font=font, fg=col1, bg=col2)
 btn2 = Button(text='Добавить нового пользователя', command=page2, font=font, fg=col1, bg=col2)
 btn_test = Button(text='Сделать фото с вебки', font=font, fg=col1, bg=col2, command=recognize)
-page1 = [btn1, btn2, btn_test]
+page1 = [btn1, btn2, ras_lab, btn_test]
 
-for el in page1:
-    print(el)
-    el.pack()
+pack_page1()
 
 #Страница №2
 user_f_o = StringVar()
@@ -176,10 +202,15 @@ page2 = [entry_name, entry_surname, user_lab,btn5, dir_lab, btn4]
 
 
 #Страница №3
+del_btn = Button(text='Удалить ученика', font=font, fg=col1, bg=col2, command=delete)
 sb = Scrollbar(orient=VERTICAL)
 base = Listbox(width=93)
-page3 = [base]
+del_lab = Label(text='Потом доделаю', font=font)
+page3 = [del_lab, del_btn, base, sb]
 
 #Отрисовка
 window.mainloop()
 
+
+
+#biba  200 strok
